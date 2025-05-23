25ce67b63d2b19893d12697d6a66a424d1fb605a
J-Breno <joao.breno85@hotmail.com>
2025-05-22 23:09:58 -0300

    Enviando os pedidos para RabbitMQ

--- rabbitmq/intro-rabbit/pedidos-api/src/main/java/com/github/jbreno/pedidos/api/controllers/PedidoController.java
+++ rabbitmq/intro-rabbit/pedidos-api/src/main/java/com/github/jbreno/pedidos/api/controllers/PedidoController.java
@@ -1,6 +1,7 @@
 package com.github.jbreno.pedidos.api.controllers;
 
 import com.github.jbreno.pedidos.api.entities.Pedido;
+import com.github.jbreno.pedidos.api.service.PedidoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "api/v1/pedidos")
 public class PedidoController {
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+
+    @Autowired
+    private PedidoService pedidoService;
 
     @Operation(
             summary = "Cria um novo pedido",
@@ -30,6 +35,7 @@ public class PedidoController {
     @PostMapping
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
         logger.info("Pedido recebido: {}", pedido.toString());
+        pedidoService.enfileirarPedido(pedido);
         return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
     }
 }
